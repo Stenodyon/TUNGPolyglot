0a08aee05d9ea89d0887ba7a9c717519d1ab1958
Stenodyon <stenodyon@gmail.com>
2018-01-21 15:06:58 +0100

    Started work on Client class + debugging code

---
+++ Client.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+namespace Polyglot
+{
+    class Client
+    {
+        private string savesPath;
+
+        public Client()
+        {
+            Console.RegisterCommand("connect", this.Command_connect);
+            Console.RegisterCommand("disconnect", this.Command_disconnect);
+            savesPath = Application.persistentDataPath + "/saves/multiplayer";
+        }
+
+        private void DeleteSave()
+        {
+            string saveName = savesPath + "/_";
+            if (File.Exists(saveName))
+                File.Delete(saveName);
+        }
+
+        private void Connect()
+        {
+            if (!Directory.Exists(savesPath))
+                Directory.CreateDirectory(savesPath);
+            DeleteSave();
+            SaveManager.SaveName = "multiplayer/_";
+            SceneManager.LoadScene("gameplay");
+        }
+
+        private void Disconnect()
+        {
+            UIManager.UnlockMouseAndDisableFirstPersonLooking();
+            SceneManager.LoadScene("main menu");
+        }
+
+        private void Command_connect(IEnumerable<string> args)
+        {
+            Console.Log("Connecting...");
+            Connect();
+        }
+
+        private void Command_disconnect(IEnumerable<string> args)
+        {
+            Console.Log("Disconnecting...");
+            Disconnect();
+        }
+    }
+}
--- Polyglot.cs
+++ Polyglot.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using PiTung_Bootstrap;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Polyglot
 {
@@ -10,26 +12,57 @@ namespace Polyglot
         public override string ModAuthor => "Stenodyon";
         public override Version ModVersion => new Version(0, 1, 0);
 
+        private Client client;
+
         public override void AfterPatch()
         {
             Console.Init();
+            Console.RegisterCommand("camcount", Command_camcount);
+            client = new Client();
             Console.Log($"Polyglot v{ModVersion.ToString()} initialized");
         }
 
         public override void Update()
         {
             Console.Update();
+
+            if(Input.GetKeyDown(KeyCode.M))
+            {
+                UnityEngine.Object[] objs = GameObject.FindObjectsOfType(typeof(DummyComponent));
+                int count = objs.Length;
+                Console.Log($"There are {count} dummycomponents in this scene");
+                Command_camcount(new string[0]);
+            }
         }
 
         public override void OnGUI()
         {
-            if(ModUtilities.IsOnMainMenu)
+            if (ModUtilities.IsOnMainMenu)
             {
                 string text = "Polyglot Loaded (TAB for console)";
                 ModUtilities.Graphics.DrawText(text, new Vector2(6, 21), Color.black);
                 ModUtilities.Graphics.DrawText(text, new Vector2(5, 20), Color.white);
             }
             Console.Draw();
+        }
+
+        private static int CountCameras(Scene scene)
+        {
+            int count = 0;
+            GameObject[] rootObjs = scene.GetRootGameObjects();
+            foreach(GameObject obj in rootObjs)
+            {
+                Camera cam = obj.GetComponent<Camera>();
+                if (cam != null)
+                    count += 1;
+            }
+            return count;
+        }
+
+        private static void Command_camcount(IEnumerable<string> args)
+        {
+            int count = CountCameras(SceneManager.GetActiveScene());
+            Console.Log($"There are {count} cameras in this scene");
         }
     }
 }
