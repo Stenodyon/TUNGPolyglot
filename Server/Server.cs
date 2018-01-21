e161a913fc9e730323c64c1f628563710793da76
Stenodyon <stenodyon@gmail.com>
2018-01-21 16:49:50 +0100

    Reorganized repo, added server skeletton, readded .sln

---
+++ Server/Server.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyglotServer
+{
+    class Server
+    {
+        private TcpListener server;
+        private bool started = false;
+
+        public Server(int port)
+        {
+            IPAddress address = IPAddress.Parse("0.0.0.0");
+            server = new TcpListener(address, port);
+            server.Start();
+            started = true;
+        }
+
+        private void Accept()
+        {
+            while(started)
+            {
+                Socket socket = server.AcceptSocket();
+                Console.WriteLine($"Connection received from {socket.RemoteEndPoint}");
+                (new Thread(() => this.Serve(socket))).Start();
+            }
+        }
+
+        private void Serve(Socket socket)
+        {
+        }
+
+        public void Stop()
+        {
+            started = false;
+            server.Stop();
+        }
+    }
+}
