de2dc823962ac346e00125bc0b33b699f7bafe40
Stenodyon <stenodyon@gmail.com>
2018-02-17 15:53:09 +0100

    Custom packet deserialization

--- PolyglotCommon/Packet.cs
+++ PolyglotCommon/Packet.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization;
@@ -11,6 +12,13 @@ namespace PolyglotCommon
     public abstract class Packet
     {
         public abstract Byte[] Serialize();
+
+        private static Byte[] typeBuffer = new Byte[sizeof(PacketType)];
+
+        public static PacketType GetType(Byte[] data)
+        {
+            return (PacketType)BitConverter.ToInt32(data, 0);
+        }
 
         protected static void WriteType(MemoryStream stream, PacketType type)
         {
@@ -37,9 +45,21 @@ namespace PolyglotCommon
             WriteFloat(stream, vector.z);
         }
 
+        protected static v3 ReadV3(Byte[] data, int offset)
+        {
+            float x = BitConverter.ToSingle(data, offset);
+            offset += sizeof(int);
+            float y = BitConverter.ToSingle(data, offset);
+            offset += sizeof(int);
+            float z = BitConverter.ToSingle(data, offset);
+            offset += sizeof(int);
+            return new v3(x, y, z);
+        }
+
         protected static void WriteString(MemoryStream stream, string str)
         {
             var serstr = Encoding.Unicode.GetBytes(str);
+            WriteInt(stream, serstr.Length);
             stream.Write(serstr, 0, serstr.Length);
         }
     }
@@ -72,6 +92,17 @@ namespace PolyglotCommon
             WriteInt(stream, ID);
             return stream.ToArray();
         }
+
+        public static PlayerIDAttribution Deserialize(Byte[] data)
+        {
+#if DEBUG
+            PacketType type = (PacketType)BitConverter.ToInt32(data, 0);
+            Debug.Assert(type == PacketType.IDAttribution);
+#endif
+            int offset = sizeof(PacketType);
+            int ID = BitConverter.ToInt32(data, offset);
+            return new PlayerIDAttribution(ID);
+        }
     }
 
     [Serializable]
@@ -82,6 +113,15 @@ namespace PolyglotCommon
             MemoryStream stream = new MemoryStream();
             WriteType(stream, PacketType.IDAttributionAck);
             return stream.ToArray();
+        }
+
+        public static IDAttibutionACK Deserialize(Byte[] data)
+        {
+#if DEBUG
+            PacketType type = (PacketType)BitConverter.ToInt32(data, 0);
+            Debug.Assert(type == PacketType.IDAttributionAck);
+#endif
+            return new IDAttibutionACK();
         }
     }
 
@@ -98,6 +138,17 @@ namespace PolyglotCommon
             WriteInt(stream, ID);
             return stream.ToArray();
         }
+
+        public static NewPlayer Deserialize(Byte[] data)
+        {
+#if DEBUG
+            PacketType type = (PacketType)BitConverter.ToInt32(data, 0);
+            Debug.Assert(type == PacketType.NewPlayer);
+#endif
+            int offset = sizeof(PacketType);
+            int ID = BitConverter.ToInt32(data, offset);
+            return new NewPlayer(ID);
+        }
     }
 
     [Serializable]
@@ -112,6 +163,17 @@ namespace PolyglotCommon
             WriteType(stream, PacketType.PlayerDisconnected);
             WriteInt(stream, ID);
             return stream.ToArray();
+        }
+
+        public static PlayerDisconnected Deserialize(Byte[] data)
+        {
+#if DEBUG
+            PacketType type = (PacketType)BitConverter.ToInt32(data, 0);
+            Debug.Assert(type == PacketType.PlayerDisconnected);
+#endif
+            int offset = sizeof(PacketType);
+            int ID = BitConverter.ToInt32(data, offset);
+            return new PlayerDisconnected(ID);
         }
     }
 
@@ -138,6 +200,21 @@ namespace PolyglotCommon
             WriteV3(stream, Angles);
             return stream.ToArray();
         }
+
+        public static PlayerPosition Deserialize(Byte[] data, int offset = 0)
+        {
+#if DEBUG
+            PacketType type = (PacketType)BitConverter.ToInt32(data, offset);
+            Debug.Assert(type == PacketType.PlayerPosition);
+#endif
+            offset += sizeof(PacketType);
+            int PlayerID = BitConverter.ToInt32(data, offset);
+            offset += sizeof(int);
+            v3 Pos = ReadV3(data, offset);
+            offset += 3 * sizeof(float);
+            v3 Angles = ReadV3(data, offset);
+            return new PlayerPosition(PlayerID, Pos, Angles);
+        }
     }
 
     [Serializable]
@@ -159,6 +236,24 @@ namespace PolyglotCommon
             }
             return stream.ToArray();
         }
+
+        public static PlayerList Deserialize(Byte[] data)
+        {
+#if DEBUG
+            PacketType type = (PacketType)BitConverter.ToInt32(data, 0);
+            Debug.Assert(type == PacketType.PlayerList);
+#endif
+            int _base = sizeof(PacketType);
+            int length = BitConverter.ToInt32(data, _base);
+            PlayerPosition[] List = new PlayerPosition[length];
+            _base += sizeof(int);
+            for(int index = 0; index < length; index++)
+            {
+                int offset = _base + index * (sizeof(int) + 6 * sizeof(float));
+                List[index] = PlayerPosition.Deserialize(data, offset);
+            }
+            return new PlayerList(List);
+        }
     }
 
     [Serializable]
@@ -173,6 +268,19 @@ namespace PolyglotCommon
             WriteType(stream, PacketType.Disconnect);
             WriteString(stream, Reason);
             return stream.ToArray();
+        }
+
+        public static DisconnectPacket Deserialize(Byte[] data)
+        {
+#if DEBUG
+            PacketType type = (PacketType)BitConverter.ToInt32(data, 0);
+            Debug.Assert(type == PacketType.Disconnect);
+#endif
+            int offset = sizeof(PacketType);
+            int length = BitConverter.ToInt32(data, offset);
+            offset += sizeof(int);
+            string reason = Encoding.Unicode.GetString(data, offset, length);
+            return new DisconnectPacket(reason);
         }
     }
 
@@ -194,6 +302,33 @@ namespace PolyglotCommon
             WriteInt(stream, Height);
             return stream.ToArray();
         }
+
+        public static NewBoard Deserialize(Byte[] data)
+        {
+#if DEBUG
+            PacketType type = (PacketType)BitConverter.ToInt32(data, 0);
+            Debug.Assert(type == PacketType.NewBoard);
+#endif
+            int offset = sizeof(PacketType);
+            int ID = BitConverter.ToInt32(data, offset);
+            offset += sizeof(int);
+            int Parent = BitConverter.ToInt32(data, offset);
+            offset += sizeof(int);
+            int Width = BitConverter.ToInt32(data, offset);
+            offset += sizeof(int);
+            int Height = BitConverter.ToInt32(data, offset);
+            offset += sizeof(int);
+            v3 Position = ReadV3(data, offset);
+            offset += 3 * sizeof(float);
+            v3 Angles = ReadV3(data, offset);
+            return new NewBoard
+            {
+                ID = ID,
+                Parent = Parent,
+                Width = Width, Height = Height,
+                Position = Position, Angles = Angles
+            };
+        }
     }
 
     [Serializable]
@@ -209,6 +344,22 @@ namespace PolyglotCommon
             WriteInt(stream, GlobalID);
             return stream.ToArray();
         }
+
+        public static GlobalIDAttribution Deserialize(Byte[] data)
+        {
+#if DEBUG
+            PacketType type = (PacketType)BitConverter.ToInt32(data, 0);
+            Debug.Assert(type == PacketType.GlobalIDAttribution);
+#endif
+            int offset = sizeof(PacketType);
+            int LocalID = BitConverter.ToInt32(data, offset);
+            offset += sizeof(int);
+            int GlobalID = BitConverter.ToInt32(data, offset);
+            return new GlobalIDAttribution
+            {
+                LocalID = LocalID, GlobalID = GlobalID
+            };
+        }
     }
 
     [Serializable]
@@ -222,6 +373,17 @@ namespace PolyglotCommon
             WriteType(stream, PacketType.DeleteBoard);
             WriteInt(stream, ID);
             return stream.ToArray();
+        }
+
+        public static DeleteBoard Deserialize(Byte[] data)
+        {
+#if DEBUG
+            PacketType type = (PacketType)BitConverter.ToInt32(data, 0);
+            Debug.Assert(type == PacketType.DeleteBoard);
+#endif
+            int offset = sizeof(PacketType);
+            int ID = BitConverter.ToInt32(data, offset);
+            return new DeleteBoard { ID = ID };
         }
     }
 
@@ -241,6 +403,28 @@ namespace PolyglotCommon
             WriteV3(stream, Position);
             WriteV3(stream, Rotation);
             return stream.ToArray();
+        }
+
+        public static MovedBoard Deserialize(Byte[] data)
+        {
+#if DEBUG
+            PacketType type = (PacketType)BitConverter.ToInt32(data, 0);
+            Debug.Assert(type == PacketType.MovedBoard);
+#endif
+            int offset = sizeof(PacketType);
+            int ID = BitConverter.ToInt32(data, offset);
+            offset += sizeof(int);
+            int Parent = BitConverter.ToInt32(data, offset);
+            offset += sizeof(int);
+            v3 Position = ReadV3(data, offset);
+            offset += 3 * sizeof(float);
+            v3 Rotation = ReadV3(data, offset);
+            return new MovedBoard
+            {
+                ID = ID,
+                Parent = Parent,
+                Position = Position, Rotation = Rotation
+            };
         }
     }
 }
